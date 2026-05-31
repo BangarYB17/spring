41cf3ade9a3adb25ce90626d095c8c5db6f68f95
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-05-31 11:00:44 +0530

    Constructor Ambiguity Problem solve using--> 1)index 2)type 3)name

---
+++ ConstructorAmbiguity/src/com/model/Person.java
@@ -0,0 +1,17 @@
+package com.model;
+
+public class Person {
+
+	public Person(String name, int id) {
+		System.out.println("Constructor Executed");
+	}
+    
+	public Person(double salary ,double id) {
+		System.out.println("double Constructor Executed");
+	}
+	
+	public Person(String Address , String jobrole) {
+		System.out.println("String Constructor Executed");
+	}
+
+}
