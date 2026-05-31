8deaaa6315825f63adc3a64b8cf27e09073d2b1c
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-05-31 10:57:57 +0530

    Configuration Types: 1)value as attribute 2)value as field 3)value as schema

---
+++ ConfigurationTypes/src/com/app/App.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.model.User;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		User u = (User)context.getBean("u1");
+		u.show();
+
+	}
+
+}
